f440cc79559c5dd655aaa2d93286b77d6ab75cb3
Celso França Neto <celso.franca.neto@gmail.com>
2023-07-04 00:25:07 -0300

    Criaçaõ da base de dados

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -19,3 +19,69 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+    
+class Endereco(models.Model):
+    cep = models.CharField(max_length=8)
+    rua = models.CharField(max_length=30)
+    numero = models.IntegerField()
+    bairro = models.CharField(max_length=30)
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=30)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=13)
+    email = models.CharField(max_length=45)
+    endereco = models.ManyToManyField(Endereco)
+
+class ItemDoMenu(models.Model):
+    nome = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=100)
+    preco = models.FloatField()
+
+class Pizza(ItemDoMenu):
+    tamanho = models.IntegerField()
+    bordaRecheada = models.BooleanField()
+
+class Bebida(ItemDoMenu):
+    tamanho = models.IntegerField()
+
+class Cardapio(models.Model):
+    ItemsDoMenu = models.ManyToManyField(ItemDoMenu)
+    ativo = models.BinaryField()
+
+class Carrinho(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    itensDoMenu = models.ManyToManyField(ItemDoMenu)
+
+class ItemPedido(models.Model):
+    itemDoMenu = models.ManyToManyField(ItemDoMenu)
+    desconto = models.FloatField()
+
+class Pedido(models.Model):
+    cliente = models.ManyToManyField(Cliente)
+    itemsDePedido = models.ManyToManyField(ItemPedido)
+    data = models.DateField()
+    STATUS_CHOICES = (
+        ('FINALIZADO', 'Finalizado'),
+        ('A_CAMINHO', 'A caminho'),
+        ('PREPARANDO_PEDIDO', 'Preparando pedido'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+class Restaurante(models.Model):
+    cardapio = models.ManyToManyField(Cardapio)
+    carrinho = models.ManyToManyField(Carrinho)
+    
+
+##dsdalç
+    
+
+
+
+
+
+
+
+
+
+
