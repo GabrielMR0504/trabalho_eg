94846cb0e48909adde95ac5d78a061ba3c851c29
GabrielMR0504 <gabrielmadermr@gmail.com>
2023-07-04 00:49:03 -0300

    Adicionando tabelas ao admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import *
 admin.site.register(Choice)
 admin.site.register(Question)
+admin.site.register(Endereco)
+admin.site.register(Cliente)
+admin.site.register(ItemDoMenu)
+admin.site.register(Pizza)
+admin.site.register(Cardapio)
+admin.site.register(Carrinho)
+admin.site.register(ItemPedido)
+admin.site.register(Pedido)
+admin.site.register(Restaurante)
 # Register your models here.
